a5479478198d788b137459f802a17132ce1482dc
Kukuh Cikal <71339508+Furizou@users.noreply.github.com>
2024-11-13 16:57:44 +0700

    feat: authentication to fetch data

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, edit_mood, delete_mood, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import create_mood_flutter, show_main, create_mood_entry, edit_mood, delete_mood, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user, add_mood_entry_ajax
-
+    
 app_name = 'main'
 
 urlpatterns = [
@@ -17,4 +17,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'), # sesuaikan dengan nama fungsi yang dibuat
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -28,7 +28,7 @@ PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kukuh-cikal-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kukuh-cikal-mentalhealthtracker.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
@@ -133,3 +136,11 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://kukuh-cikal-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://kukuh-cikal-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+
+# Auth in Tutorial 8
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
